09b064a17ac5759387140b253fee8061e6860e89
edoRacca <edoraccanello@gmail.com>
2025-10-24 12:44:47 +0200

    wordpress entrypoint

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+set -e WP_PATH="/var/www/html"
+
+if [ ! -f $WP_PATH/wp-config.php ] then
+	echo "Installing WordPress ..."
+	wp core download --path="$WP_PATH" --allow-root
+else
+	echo "WordPress already installed"
+fi
